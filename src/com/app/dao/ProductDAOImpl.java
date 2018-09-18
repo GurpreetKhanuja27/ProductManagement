72cb92605499537eb7da207ad37c25ec187e5f94
GurpreetKhanuja27 <38744459+GurpreetKhanuja27@users.noreply.github.com>
2018-09-18 13:23:35 -0400

    Update ProductDAOImpl.java
    Changed from List to Set

--- src/com/app/dao/ProductDAOImpl.java
+++ src/com/app/dao/ProductDAOImpl.java
@@ -7,7 +7,7 @@ import com.app.bean.Product;
 public class ProductDAOImpl implements ProductDAO{
 
 	@Override
-	public List<Product> listProducts() {
+	public Set<Product> listProducts() {
 		// TODO Auto-generated method stub
 		return null;
 	}
